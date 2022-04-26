28381f8e1762f6fe5e846bd88e967988f8a721fa
JohnOiko <ioannisoa@csd.auth.gr>
2022-04-26 10:58:44 +0300

    Finished the checkout page.

--- JS/checkoutpage.js
+++ JS/checkoutpage.js
@@ -1,3 +1,22 @@
+function updateShippingAddress() {
+    console.log(document.getElementById("same_adr").checked)
+    if (document.getElementById("same_adr").checked) {
+        document.getElementById("ship_det").style.display = "none"
+        document.getElementById("ship_adr").required = false
+        document.getElementById("ship_city").required = false
+        document.getElementById("ship_state").required = false
+        document.getElementById("ship_zip").required = false
+    }
+    else {
+        document.getElementById("ship_det").style.display = "flex"
+        document.getElementById("ship_det").style.visibility = "none"
+        document.getElementById("ship_adr").required = true
+        document.getElementById("ship_city").required = true
+        document.getElementById("ship_state").required = true
+        document.getElementById("ship_zip").required = true
+    }
+}
+
 function updateOrderModal() {
     document.getElementById("modal_fname").innerHTML = document.getElementById("fname").value
     document.getElementById("modal_email").innerHTML = document.getElementById("email").value
