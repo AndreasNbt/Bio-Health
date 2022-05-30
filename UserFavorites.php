bba9749469074712495b4d5d455765c33e1c56b2
JohnOiko <ioannisoa@csd.auth.gr>
2022-05-30 20:45:10 +0300

    Updated user favorites

--- UserFavorites.php
+++ UserFavorites.php
@@ -16,132 +16,160 @@
     <script src="https://kit.fontawesome.com/61e165c770.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="CSS/searchpage.css">
     <script src="JS/search.js"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="JS/addToFavourites.js"></script>
-    
 </head>
 <body class="grey-background d-flex flex-column min-vh-100">
     
-    <?php include "Navbar.php";
-        $product_ids = array();
-        $products = array();
-        $user_id = $_SESSION['ID'];
-
-        include("PHP_Back_End/db_connection.php");
-
-        $sql = "SELECT product.id, name, price, stock, image FROM product, user_favourites WHERE user_favourites.user_id='$user_id' AND product.id=user_favourites.product_id;";
-        $res = $con->query($sql);
-        
-        while($row = mysqli_fetch_row($res)){
-            $products[$row[0]]['id'] = $row[0];
-            $products[$row[0]]['name'] = $row[1];
-            $products[$row[0]]['price'] = $row[2];
-            $products[$row[0]]['stock'] = $row[3];
-            $products[$row[0]]['image'] = $row[4];
-        }
-
-        mysqli_close($con);
-
-    ?>
+    <?php include "Navbar.php"; ?>
     <br>
 
-    <div class="container-fluid left-right-pad">
-        <table style="margin:auto">
-                <?php
-                $index = 0;
-                foreach( $products as $product){
-                    if( $index === 0 || ($index % 3 === 0 ) ){
-                        echo "<tr>";
-                        $tr = true;
-                    }
-                    $index += 1;
-                ?>
-                <?php
-                    $id = $product['id'];
-                    echo "<td id='product$id' class='favorite-product'>"
-                ?>
-                    <a href="UserProductInfo.php?productID=<?php echo $product['id']; ?>"><img src="<?php echo $product['image']; ?>" style="width:100%"></a>
-                    <h5>
-                        <?php
-                        $id = $product['id'];
-                        echo "name: <span id='name$id'>".$product['name']."</span></br>";
-                        echo "price: <span id='price$id'>".$product['price']."</span></br>";
-                        echo "stock: <span id='stock$id'>".$product['stock']."</span></br>";
-                        ?>
-                    </h5>
+    <div class="container-fluid left-right-pad" style="padding-top: 2rem">
+        <div id="products" class="grid-justify-content-evenly">
+        <?php
+        function echoUserProduct($id) {
 
-                    <?php
+            include("PHP_Back_End/db_connection.php");
 
-                    $hidden = "";
-                    if ($product['stock'] <= 0) {
-                        $hidden = "hidden ";
-                    }
+            $sql = "SELECT name, FORMAT(price, 2), stock, image, category_id FROM `product` WHERE id=$id;";
+            $res = $con->query($sql);
+            $product = mysqli_fetch_row($res);
+            $name = $product[0];
+            $initial_price = $product[1];
+            $stock = $product[2];
+            $img = $product[3];
+            $cat = $product[4];
 
-                    $id = $product['id'];
-                    $name = $product['name'];
-                    $price = $product['price'];
-                    $stock = $product['stock'];
-                    $amount = 0;
+            $sql = "SELECT icon FROM `category` WHERE id=$cat;";
+            $res = $con->query($sql);
+            $icon = mysqli_fetch_row($res)[0];
+            $linkToProductInfo = "UserProductInfo.php"."?productID=$id";
 
-                    include("PHP_Back_End/db_connection.php");
+            $offer_hidden = " hidden";
+            $price = $initial_price;
+            $offers_icon = "sources/images/category_icons/offers_icon.png";
 
-                    if (isset($_SESSION['ID']) and $_SESSION['role'] === "Customer") {
-                        $user_id = $_SESSION['ID'];
-                        $sql = "SELECT amount FROM cart_item WHERE product_id=$id AND user_id='$user_id';";
-                        $res = $con->query($sql);
-                        if (mysqli_num_rows($res) > 0) {
-                            $amount = mysqli_fetch_row($res)[0];
-                        }
-                    }
-                    $totalCost = $amount * $price;
+            $name_col_width = "col-9";
+            $price_col_width = "col-3 text-end pt-3";
+            $res = $con->query("SELECT offer_percentage FROM offers WHERE product_id = $id");
+            if (mysqli_num_rows($res) > 0) {
+                $percentage = mysqli_fetch_row($res)[0];
+                $price = number_format($initial_price - ($percentage/100)*$initial_price,2);
+                $offer_hidden = "";
+                $name_col_width = "col-7";
+                $price_col_width = "col-5 text-end pt-3";
+            }
 
-                    mysqli_close($con);
+            $amount = 0;
+            $hidden = "";
+            if (isset($_SESSION['ID'])) {
+                $user_id = $_SESSION['ID'];
+                $sql = "SELECT amount FROM cart_item WHERE product_id=$id AND user_id='$user_id';";
+                $res = $con->query($sql);
+                if (mysqli_num_rows($res) > 0) {
+                    $amount = mysqli_fetch_row($res)[0];
+                }
+            }
+            else {
+                $hidden = "hidden ";
+            }
+            $totalCost = $amount * $price;
+            $totalCost = number_format($totalCost, 2);
 
-                    echo "<button {$hidden}type='button' class='add-btn light-green' data-bs-toggle='modal' data-bs-target='#addToCartModal$id' onclick='saveValues(\"$id\")'>Add to cart</button>
-                                  <div class='modal fade' id='addToCartModal$id'>
-                                       <div class='modal-dialog modal-lg'>
-                                           <div class='modal-content'>
-            
-                                               <div class='modal-header'>
-                                                   <h3 class='modal-title' style='padding-left: 1.5rem'>Add $name to your cart</h3>
-                                                   <button type='button' style='padding-right: 1.5rem' class='btn-close' data-bs-dismiss='modal'></button>
-                                               </div>
-            
-                                               <div class='modal-body'>
-                                                   <div class='container-fluid'>
-                                                       <div class='row'>
-                                                           <div class='col-6'>
-                                                               <p class='text-lg'>Product name: $name</p>
-                                                               <p class='text-lg'>Available stock: $stock</p>
-                                                               <p class='text-lg' style='font-weight: bold'>Cost: <span id='costAddedToCart$id'>{$totalCost}€</span></p>
-                                                           </div>
-            
-                                                           <div class='col-6'>
-                                                               <label class='form-label float-end text-lg' for='amountAddedToCart$id'>Amount to be added to cart:</label>
-                                                               <input type='number' id='amountAddedToCart$id' class='form-control float-end text-lg' min='0' max='$stock' style='width: 12.5rem' value='$amount' onchange='updateCost($id)'/>
+            $new_products_icon_hidden = " hidden";
+            $new_products_icon = "sources/images/category_icons/new_products_icon.png";
+            $sql = "SELECT product_id FROM new WHERE product_id = $id";
+            $res = $con->query($sql);
+            if (mysqli_num_rows($res) > 0) {
+                $new_products_icon_hidden = "";
+            }
+
+            mysqli_close($con);
+
+            echo "<div class='container-fluid' id='product$id' style='padding: 0'>
+    
+                    <!-- link and image -->
+                    <a href='$linkToProductInfo'>
+                        <img class='img-responsive rounded' src='$img' style='width: 250px;height: 250px' alt='Picture of $name'>
+                    </a>
+    
+                    <!-- description and price -->
+                    <div class='row'>
+                        <div class='$name_col_width'>
+                            <a href='$linkToProductInfo' style='font-size: 14px; word-break: break-word'>$name<br/>(<span id='stock$id'>$stock</span> left in stock)</a>
+                        </div>
+                        <div class='$price_col_width'>
+                            <p class='product-info' style='font-size: 14px'><span$offer_hidden><span style='color:red;text-decoration:line-through'><span style='color:#212529'>{$initial_price}€</span></span>&nbsp</span><span id='price$id'>$price</span>€</p>
+                        </div>
+                    </div>
+    
+                    <!-- categories and cart button -->
+                    <div class='row' style='padding-left: 0.75rem; padding-right: 0'>
+                        <div class='col-auto d-flex flex-wrap align-items-center' style='padding-left: 0;padding-right: 0.25rem;padding-top: 0.25rem'>
+                            <img class='img-responsive' src='$icon' style='width: 20px;height: 20px' alt='Product Name'>
+                            <img$offer_hidden class='img-responsive ps-1' src='$offers_icon' style='width: 23px' alt='Product Name'>
+                            <img$new_products_icon_hidden class='img-responsive ps-1' src='$new_products_icon' style='width: 23px' alt='Product Name'>
+                        </div>
+                        
+                        <div class='col float-end'>
+                            <div class='row'>
+                                <div class='d-flex flex-row-reverse'>
+                                    <button type='button' class='btn btn-outline-danger float-end in-search ms-2' onclick='removeFromFavorites(\"$id\")'>Remove</button>
+                                    <button {$hidden}type='button' class='btn btn-outline-primary btn-lg float-end' data-bs-toggle='modal' data-bs-target='#addToCartModal$id' onclick='saveValues(\"$id\")'>Add</button>
+                                    <div class='modal fade' id='addToCartModal$id'>
+                                           <div class='modal-dialog modal-lg'>
+                                               <div class='modal-content'>
+                
+                                                   <div class='modal-header'>
+                                                       <h3 class='modal-title' style='padding-left: 1.5rem'>Add $name to your cart</h3>
+                                                       <button type='button' style='padding-right: 1.5rem' class='btn-close' data-bs-dismiss='modal'></button>
+                                                   </div>
+                
+                                                   <div class='modal-body'>
+                                                       <div class='container-fluid'>
+                                                           <div class='row'>
+                                                               <div class='col-6'>
+                                                                   <p class='text-lg'>Product name: $name</p>
+                                                                   <p class='text-lg'>Available stock: $stock</p>
+                                                                   <p class='text-lg' style='font-weight: bold'>Cost: <span id='costAddedToCart$id'>{$totalCost}€</span></p>
+                                                               </div>
+                
+                                                               <div class='col-6'>
+                                                                   <label class='form-label float-end text-lg' for='amountAddedToCart$id'>Amount to be added to cart:</label>
+                                                                   <input type='number' id='amountAddedToCart$id' class='form-control float-end text-lg' min='0' max='$stock' style='width: 12.5rem' value='$amount' onchange='updateCost($id)'/>
+                                                               </div>
                                                            </div>
                                                        </div>
                                                    </div>
+                
+                                                   <div class='modal-footer'>
+                                                       <button type='button' class='btn btn-outline-success text-lg' id='finishCartButton$id' data-bs-dismiss='modal' onclick='addToCart(\"$id\")'>Finish</button>
+                                                       <button type='button' class='btn btn-outline-danger text-lg' data-bs-dismiss='modal' onclick='restoreValues(\"$id\")'>Cancel</button>
+                                                   </div>
+                
                                                </div>
-            
-                                               <div class='modal-footer'>
-                                                   <button type='button' class='btn btn-outline-success text-lg' id='finishCartButton$id' data-bs-dismiss='modal' onclick='addToCart(\"$id\")'>Finish</button>
-                                                   <button type='button' class='btn btn-outline-danger text-lg' data-bs-dismiss='modal' onclick='restoreValues(\"$id\")'>Cancel</button>
-                                               </div>
-            
                                            </div>
-                                       </div>
-                                  </div>";
-                    echo "<button class='button sign-btn solid-border dark-green extra-spacing mt-3' style='width: 60%' onclick='removeFromFavorites(\"$id\")'>Remove</button>"
-                    ?>
-                </td>
-                <?php if($tr){ 
-                    "</tr>";
-                    }
-                } 
-                ?>
-        </table>
+                                      </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+        }
 
+        include("PHP_Back_End/db_connection.php");
+
+        $user_id = $_SESSION['ID'];
+        $sql = "SELECT product.id FROM product, user_favourites WHERE user_favourites.user_id='$user_id' AND product.id=user_favourites.product_id;";
+        $res = $con->query($sql);
+
+        while ($row = mysqli_fetch_array($res)) {
+            echoUserProduct($row[0]);
+        }
+        mysqli_close($con);
+
+        ?>
+        </div>
     </div>
 
     <?php 
